15cca00f2a19aeeb1fd131c6c63079e3d9437cdd
Akash Dey <akashdey4123@gmail.com>
2026-09-12 23:24:40 +0600

    fix: dynamic base path for Netlify vs GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-   base: "/Assingment-5-Dev-Stack-/",
+  base: process.env.NETLIFY ? "/" : "/Assingment-5-Dev-Stack-/",
 });
